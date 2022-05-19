f451aad498fcf13e7776f9a2d8dfd4b3513f1a72
Vivian Chen <vnchen92@gmail.com>
2022-05-19 14:43:39 -0400

    Created our first clock

---
+++ intro_callbacks.js
@@ -0,0 +1,32 @@
+class Clock {
+    constructor() {
+        let date = new Date();
+        this.seconds = date.getSeconds();
+        this.minutes = date.getMinutes();
+        this.hours = date.getHours();
+
+        this.printTime();
+
+        setInterval(this._tick.bind(this), 1000);
+    }
+
+    printTime() {
+        let time = `${this.hours}:${this.minutes}:${this.seconds}`;
+        console.log(time);
+    }
+
+    _tick() {
+        if (this.seconds < 59 && this.minutes < 59 ) {
+            this.seconds++;
+        } else if (this.minutes < 59) {
+            this.seconds = 0;
+            this.minutes++;
+        } else {
+            this.hours++;
+            this.seconds = 0;
+            this.minutes = 0;
+        }
+        console.log(this.printTime());
+    }
+}
+
